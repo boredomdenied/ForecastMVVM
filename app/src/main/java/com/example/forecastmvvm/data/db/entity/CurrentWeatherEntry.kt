0c19ebce44407f8fcc03a74e95e3366a49b148ec
boredomdenied <brandon@boredomdenied.com>
2019-04-30 19:52:10 -0500

    Retrofit testing successful, implementing Room

---
+++ app/src/main/java/com/example/forecastmvvm/data/db/entity/CurrentWeatherEntry.kt
@@ -0,0 +1,39 @@
+package com.example.forecastmvvm.data.db.entity
+
+
+import androidx.room.Entity
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "current_weather")
+data class CurrentWeatherEntry(
+    val condition: Condition,
+    @SerializedName("feelslike_c")
+    val feelslikeC: Double,
+    @SerializedName("feelslike_f")
+    val feelslikeF: Double,
+    @SerializedName("gust_kph")
+    val gustKph: Double,
+    @SerializedName("gust_mph")
+    val gustMph: Double,
+    @SerializedName("is_day")
+    val isDay: Int,
+    @SerializedName("precip_in")
+    val precipIn: Double,
+    @SerializedName("precip_mm")
+    val precipMm: Double,
+    @SerializedName("temp_c")
+    val tempC: Double,
+    @SerializedName("temp_f")
+    val tempF: Double,
+    val uv: Double,
+    @SerializedName("vis_km")
+    val visKm: Double,
+    @SerializedName("vis_miles")
+    val visMiles: Double,
+    @SerializedName("wind_dir")
+    val windDir: String,
+    @SerializedName("wind_kph")
+    val windKph: Double,
+    @SerializedName("wind_mph")
+    val windMph: Double
+)
---
+++ app/src/main/java/com/example/forecastmvvm/network/response/CurrentWeatherResponse.kt
@@ -0,0 +1,12 @@
+package com.example.forecastmvvm.network.response
+
+import com.example.forecastmvvm.data.db.entity.CurrentWeatherEntry
+import com.example.forecastmvvm.data.db.entity.Location
+import com.google.gson.annotations.SerializedName
+
+
+data class CurrentWeatherResponse(
+    @SerializedName("current")
+    val currentWeatherEntry: CurrentWeatherEntry,
+    val location: Location
+)
